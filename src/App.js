4b008dfe6b60b4f5f9c1afdc52eb50cacbb3b5ab
Jason A. Wise <jasonandrewwise@gmail.com>
2021-06-28 22:23:36 -0400

    - adding line height - adding content to cta

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import CallToAction from './Components/CallToAction';
 function App() {
   return (
     <div className="flex sm:flex-col items-center">
-      <CallToAction heading="I am the call to action" />
+      <CallToAction
+        heading="I am the call to action"
+        content="Sign Up For My Newsletter"
+      />
       <div>Form</div>
     </div>
   );
--- src/Components/CallToAction.js
+++ src/Components/CallToAction.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CallToAction = ({ heading }) => (
-  <div>
+const CallToAction = ({ heading, content }) => (
+  <div className="text-center">
     <h1>{heading}</h1>
+    <p>{content}</p>
   </div>
 );
 
 CallToAction.propTypes = {
   heading: PropTypes.string.isRequired,
+  content: PropTypes.string,
 };
 
 export default CallToAction;
